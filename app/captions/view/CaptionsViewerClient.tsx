a8738323f17cd297d0e5f9094a522b8d3ae926a0
toblilee <tolbihunter@gmail.com>
2026-05-03 03:34:46 +0900

    Tune yayeon caption background overlay

--- app/captions/view/CaptionsViewerClient.tsx
+++ app/captions/view/CaptionsViewerClient.tsx
@@ -264,9 +264,9 @@ export default function CaptionsViewerClient({ initialRoomCode }: { initialRoomC
       {isYayeonTheme ? (
         <div className="pointer-events-none absolute inset-0">
           <div className="absolute inset-0 bg-[url('/images/yayeon-bg.jpg')] bg-cover bg-center" />
-          <div className="absolute inset-0 bg-gradient-to-br from-[#102047]/55 via-[#5c4f93]/35 to-[#7db9e3]/25" />
-          <div className="absolute -left-24 top-24 h-72 w-72 rounded-full bg-[#ce93b3]/30 blur-3xl" />
-          <div className="absolute -right-20 bottom-16 h-80 w-80 rounded-full bg-[#7db9e3]/30 blur-3xl" />
+          <div className="absolute inset-0 bg-gradient-to-br from-[#102047]/20 via-[#5c4f93]/12 to-[#7db9e3]/8" />
+          <div className="absolute -left-24 top-24 h-72 w-72 rounded-full bg-[#ce93b3]/10 blur-3xl" />
+          <div className="absolute -right-20 bottom-16 h-80 w-80 rounded-full bg-[#7db9e3]/10 blur-3xl" />
         </div>
       ) : null}
       <header className={headerClassName}>
